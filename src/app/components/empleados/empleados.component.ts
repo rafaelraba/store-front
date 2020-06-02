69b3c520ca5a67c82e1bcfed68c3ee5557df54ab
kmilo muñoz <mckamilo@gmail.com>
2020-06-02 05:20:22 -0500

    Servicio creado2

--- src/app/components/empleados/empleados.component.ts
+++ src/app/components/empleados/empleados.component.ts
@@ -7,13 +7,16 @@ import { GeneralServiceService } from '../../service/general-service.service';
   styleUrls: ['./empleados.component.scss'],
 })
 export class EmpleadosComponent implements OnInit {
+  public respuesta: string;
   constructor(private service: GeneralServiceService) {
     this.inicioMetodos();
   }
 
   ngOnInit(): void {}
   private inicioMetodos() {
+    this.respuesta = '';
     this.service.pruebaService().subscribe((data) => {
+      this.respuesta = data;
       console.log(data);
     });
   }
