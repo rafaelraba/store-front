ab628298dfcaf91854744df48f6abb666e122225
Cristian <crmunozp2@poligran.edu.co>
2020-07-04 17:29:07 -0500

    Implementar modulos ventas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'empleado', component: EmpleadosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: '**', component: AppComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
